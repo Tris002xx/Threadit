234d4e2012363df57062b49dc3cb267e79b06c93
tris002xx <tristanjames3131@gmail.com>
2024-08-03 17:13:36 -0700

    adding log out user functionality

--- routes/login.js
+++ routes/login.js
@@ -11,6 +11,12 @@ const {
 // Routes
 router.get("/", renderLogin);
 router.post("/password", authenticateLogin, redirect);
-
-
+router.get("/logout", function (req, res, next) {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
+});
 module.exports = router;
